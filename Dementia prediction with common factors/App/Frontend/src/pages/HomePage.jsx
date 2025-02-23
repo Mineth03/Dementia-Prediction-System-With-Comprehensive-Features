93e4f264faec94e9a1c5be2736348246a9065bc5
MIneth03 <minethsandew@gmail.com>
2025-02-23 09:33:41 +0530

    Code Updates

---
+++ Dementia prediction with common factors/App/Frontend/src/pages/HomePage.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const HomePage = () => {
+  return (
+    <div>
+      <h1>Welcome to the Home Page!</h1>
+      <p>This page should display properly.</p>
+    </div>
+  );
+};
+
+export default HomePage;
