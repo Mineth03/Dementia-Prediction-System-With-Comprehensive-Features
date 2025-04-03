e393cab2879a4a3313f0a0009b9901db7d3f76fb
MIneth03 <minethsandew@gmail.com>
2025-04-03 18:13:38 +0530

    remove unwanted

--- Chatbot/App/Frontend/src/components/Header.jsx
+++
@@ -1,9 +0,0 @@
-const Header = () => {
-  return (
-    <header className="bg-blue-600 text-white p-4 text-center text-xl font-bold">
-      My Application
-    </header>
-  );
-};
-
-export default Header;
--- Tracking and recommendation/server/realtimecatching.js
+++
@@ -1,4 +0,0 @@
-import io from 'socket.io-client';
-
-// Connect to Flask-SocketIO server
-const socket = io('http://localhost:5000');  
